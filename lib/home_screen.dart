80af8885abf910d5b7dfbc8e3916396a593d6fbe
moniruzzaman76 <ariyankhanmonir555@gmail.com>
2023-07-20 12:15:45 +0600

    live test 9

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,87 +3,96 @@ import 'dart:convert';
 import 'model_class.dart';
 
 class RecipeListScreen extends StatefulWidget {
-
   const RecipeListScreen({super.key});
 
   @override
-  State<RecipeListScreen> createState() => _RecipeListScreenState();
+  _RecipeListScreenState createState() => _RecipeListScreenState();
 }
 
 class _RecipeListScreenState extends State<RecipeListScreen> {
-  final String jsonString = '''
-    {
-      "recipes": [
-        {
-          "title": "Pasta Carbonara",
-          "description": "Creamy pasta dish with bacon and cheese.",
-          "ingredients": ["spaghetti", "bacon", "egg", "cheese"]
-        },
-        {
-          "title": "Caprese Salad",
-          "description": "Simple and refreshing salad with tomatoes, mozzarella, and basil.",
-          "ingredients": ["tomatoes", "mozzarella", "basil"]
-        },
-        {
-          "title": "Banana Smoothie",
-          "description": "Healthy and creamy smoothie with bananas and milk.",
-          "ingredients": ["bananas", "milk"]
-        },
-        {
-          "title": "Chicken Stir-Fry",
-          "description": "Quick and flavorful stir-fried chicken with vegetables.",
-          "ingredients": ["chicken breast", "broccoli", "carrot", "soy sauce"]
-        },
-        {
-          "title": "Grilled Salmon",
-          "description": "Delicious grilled salmon with lemon and herbs.",
-          "ingredients": ["salmon fillet", "lemon", "olive oil", "dill"]
-        },
-        {
-          "title": "Vegetable Curry",
-          "description": "Spicy and aromatic vegetable curry.",
-          "ingredients": ["mixed vegetables", "coconut milk", "curry powder"]
-        },
-        {
-          "title": "Berry Parfait",
-          "description": "Layered dessert with fresh berries and yogurt.",
-          "ingredients": ["berries", "yogurt", "granola"]
-        }
-      ]
-    }
-  ''';
 
-  List<Recipe> _recipesFromJson(String jsonString) {
-    final parsedJson = json.decode(jsonString);
-    final recipeList = parsedJson['recipes'] as List<dynamic>;
-    return recipeList.map((recipeJson) => Recipe.fromJson(recipeJson)).toList();
+  List<Recipe> allFoodRecipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRecipesData();
+  }
+
+  void fetchRecipesData() {
+    // Simulate fetching recipes from the JSON
+    String jsonString = '''
+      {
+        "recipes": [
+          {
+            "title": "Pasta Carbonara",
+            "description": "Creamy pasta dish with bacon and cheese.",
+            "ingredients": ["spaghetti", "bacon", "egg", "cheese"]
+          },
+          {
+            "title": "Caprese Salad",
+            "description": "Simple and refreshing salad with tomatoes, mozzarella, and basil.",
+            "ingredients": ["tomatoes", "mozzarella", "basil"]
+          },
+          {
+            "title": "Banana Smoothie",
+            "description": "Healthy and creamy smoothie with bananas and milk.",
+            "ingredients": ["bananas", "milk"]
+          },
+          {
+            "title": "Chicken Stir-Fry",
+            "description": "Quick and flavorful stir-fried chicken with vegetables.",
+            "ingredients": ["chicken breast", "broccoli", "carrot", "soy sauce"]
+          },
+          {
+            "title": "Grilled Salmon",
+            "description": "Delicious grilled salmon with lemon and herbs.",
+            "ingredients": ["salmon fillet", "lemon", "olive oil", "dill"]
+          },
+          {
+            "title": "Vegetable Curry",
+            "description": "Spicy and aromatic vegetable curry.",
+            "ingredients": ["mixed vegetables", "coconut milk", "curry powder"]
+          },
+          {
+            "title": "Berry Parfait",
+            "description": "Layered dessert with fresh berries and yogurt.",
+            "ingredients": ["berries", "yogurt", "granola"]
+          }
+        ]
+      }
+    ''';
+
+    // final jsonData = json.decode(jsonString);
+    // final  List<dynamic> recipeList = jsonData['recipes'];
+    // //allFoodRecipes = recipeList.map((recipeJson) => Recipe.fromJson(recipeJson)).toList();
+
+
+    final List<dynamic>jsonData = jsonDecode(jsonString)['recipes'];
+    allFoodRecipes = jsonData.map((e) => Recipe.fromJson(e)).toList();
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final recipes = _recipesFromJson(jsonString);
-
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Food Recipes'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ListView.builder(
-          itemCount: recipes.length,
-          itemBuilder: (context, index) {
-            final recipe = recipes[index];
-            return ListTile(
-              title: Text(
-                recipe.title,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              subtitle: Text(recipe.description,style: const TextStyle(fontSize: 16),),
-            );
-          },
-        ),
+      body: ListView.builder(
+        itemCount: allFoodRecipes.length,
+        itemBuilder: (context, index) {
+          final recipe = allFoodRecipes[index];
+          return ListTile(
+            title: Text(recipe.title),
+            subtitle: Text(recipe.description),
+          );
+        },
       ),
     );
   }
 }
+
+
